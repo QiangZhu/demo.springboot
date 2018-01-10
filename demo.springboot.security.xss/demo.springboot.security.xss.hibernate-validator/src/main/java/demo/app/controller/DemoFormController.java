1dde48f9f3cb1901a3d706beffd7fe4e3a242166
QiangZhu <zhuqiang@gmail.com>
2018-01-10 11:06:41 +0800

    form表单提交的xss验证方式

--- demo.springboot.security.xss/demo.springboot.security.xss.hibernate-validator/src/main/java/demo/app/controller/DemoFormController.java
+++ demo.springboot.security.xss/demo.springboot.security.xss.hibernate-validator/src/main/java/demo/app/controller/DemoFormController.java
@@ -2,18 +2,21 @@ package demo.app.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import demo.app.dto.DemoDto;
 
-@RestController
-public class DemoController {
+@Controller
+public class DemoFormController {
 	
-	@RequestMapping(name = "/create",consumes = {"application/json;charset=utf-8"})
-	public DemoDto create(@Valid @RequestBody DemoDto demoDto) {
-		System.out.println(String.format("create dto %s", demoDto));
+	@RequestMapping(name = "/create",consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE })
+	@ResponseBody
+	public  DemoDto create(@Valid DemoDto demoDto) {
+		System.out.println(String.format("%s request %s", MediaType.APPLICATION_FORM_URLENCODED_VALUE,demoDto));
 		return demoDto;
 	}
 
