7932f79c4c65eb9091ed62d207795061d2e588a3
QiangZhu <zhuqiang@gmail.com>
2018-02-08 17:44:26 +0800

    add demo.springboot.actuactor.info.git project

---
+++ demo.springboot.actuator/demo.springboot.actuator.info.git/src/main/java/demo/App.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhuqiang on 2018-02-08.
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class,args);
+	}
+}
